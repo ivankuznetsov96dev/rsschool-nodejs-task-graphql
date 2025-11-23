1990dc908a05ade00178fefb5c03de1895266b90
Ivan Kuzniatsou <i.kuznetsov@viscur.ru>
2025-11-24 02:42:33 +0300

    feat: added creating loaders, fs handler

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,9 +1,28 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import {
+  GraphQLSchema,
+  GraphQLScalarType,
+  parse as gqlParse,
+  validate,
+  execute,
+} from 'graphql';
+import depthLimit from 'graphql-depth-limit';
+import type { PrismaClient } from '@prisma/client';
+import { UUIDType } from './types/uuid.js';
+import { Mutations } from './resolvers/mutation.js';
+import { RootQuery } from './resolvers/query.js';
+import { createLoaders } from './context.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
-  const { prisma } = fastify;
+  const prisma: PrismaClient = fastify.prisma;
+
+  const schema = new GraphQLSchema({
+    query: RootQuery,
+    mutation: Mutations,
+    types: [UUIDType as GraphQLScalarType],
+  });
+  
 
   fastify.route({
     url: '/',
@@ -14,8 +33,26 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
+    
     async handler(req) {
-      // return graphql();
+      const loaders = createLoaders(prisma);
+      const contextValue = { prisma, loaders, loadersList: loaders };
+
+      const document = gqlParse(req.body.query);
+      const validationErrors = validate(schema, document, [depthLimit(5)]);
+
+      if (validationErrors.length > 0) {
+        return { errors: validationErrors };
+      }
+
+      const result = await execute({
+        schema,
+        document,
+        contextValue,
+        variableValues: req.body.variables,
+      });
+
+      return result;
     },
   });
 };
